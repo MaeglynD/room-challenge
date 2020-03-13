1fbbb0c9355c5e6c5d5b06feba4b0093e3e17bc3
MaeglynD <31933322+MaeglynD@users.noreply.github.com>
2020-03-13 03:18:53 +0000

    init

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace room_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make our text cyan and the users white
+            void WriteMessage(string msg)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(msg);
+                Console.ResetColor();
+            }
+
+            // Setup console
+            Console.Clear();
+            Console.WriteLine(@"
+                +------+.
+                |`.    | `.
+                |  `+--+---+
+                |   |  |   |
+                +---+--+.  |  (h)
+            (l)  `. |    `.|
+                   `+------+
+                       (w)
+            ");
+        }
+    }
+}
