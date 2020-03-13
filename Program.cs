b637ecd1f692d3155579c9ca415c55acbf1fbe69
MaeglynD <31933322+MaeglynD@users.noreply.github.com>
2020-03-13 03:27:13 +0000

    progress, recursion funcs added

--- Program.cs
+++ Program.cs
@@ -15,6 +15,21 @@ namespace room_challenge
                 Console.ResetColor();
             }
 
+            // A recursive function that returns only when the user has a entered valid double
+            double ValidateDouble(string msg)
+            {
+                double ToValidate;
+                WriteMessage($"\n{msg}");
+
+                if (!double.TryParse(Console.ReadLine(), out ToValidate))
+                {
+                    WriteMessage("\nHad trouble reading that, enter a valid number (decimals are allowed)");
+                    return ValidateDouble(msg);
+                }
+
+                return ToValidate;
+            };
+
             // Setup console
             Console.Clear();
             Console.WriteLine(@"
@@ -27,6 +42,12 @@ namespace room_challenge
                    `+------+
                        (w)
             ");
+
+            // Users input
+            double paintCoats = ValidateDouble("How many coats of paints would you like?");
+            double height = ValidateDouble("Please enter your room's height (h) in metres");
+            double width = ValidateDouble("Please enter your room's width (w) in metres");
+            double length = ValidateDouble("Please enter your room's length (l) in metres");
         }
     }
 }
